2710ee8495d642965fdb9426a691675e1ec5c4a1
orangejamface <orangejamface@gmail.com>
2022-02-24 15:48:41 -0500

    added video content for sound section of site

--- java.js
+++ java.js
@@ -1,17 +1,4 @@
 
-
-// function toggle(IDS) {
-//     var sel = document.getElementById('pg').getElementsByTagName('div');
-//     for (var i=0; i<sel.length; i++) { 
-//       if (sel[i].id != IDS) { sel[i].style.display = 'none'; }
-//     }
-//     var status = document.getElementById(IDS).style.display;
-//     if (status == 'block') { document.getElementById(IDS).style.display = 'none'; }
-//                       else { document.getElementById(IDS).style.display = 'block'; }
-  
-//     return false;
-//   }
-
 function toggleSound() {
   let sound = document.getElementById("soundDesign");
   let video = document.getElementById("video");
