c2a7601a412ea28ae5158cd5c1100cf7ed2e42fb
orangejamface <orangejamface@gmail.com>
2022-02-24 14:42:55 -0500

    wrote 3 functions for 3 buttons on top of page which toggle different portfolio elements on and off, only issue is that if you click the button which corresponds to the current viewable element it will hide that element meaning none of the 3 are currently displayed,  I do not mind this, I have tried altering some of the parameters in the else part of the function but have yet to make it so that when you are on sound design for example and click the sound design button nothing happens, this is something I dont know that i actually want to change for this site, but it is something I would like to understand and know how to do

--- java.js
+++ java.js
@@ -1,13 +1,52 @@
 
 
-function toggle(IDS) {
-    var sel = document.getElementById('pg').getElementsByTagName('div');
-    for (var i=0; i<sel.length; i++) { 
-      if (sel[i].id != IDS) { sel[i].style.display = 'none'; }
-    }
-    var status = document.getElementById(IDS).style.display;
-    if (status == 'block') { document.getElementById(IDS).style.display = 'none'; }
-                      else { document.getElementById(IDS).style.display = 'block'; }
+// function toggle(IDS) {
+//     var sel = document.getElementById('pg').getElementsByTagName('div');
+//     for (var i=0; i<sel.length; i++) { 
+//       if (sel[i].id != IDS) { sel[i].style.display = 'none'; }
+//     }
+//     var status = document.getElementById(IDS).style.display;
+//     if (status == 'block') { document.getElementById(IDS).style.display = 'none'; }
+//                       else { document.getElementById(IDS).style.display = 'block'; }
   
-    return false;
-  }
+//     return false;
+//   }
+
+function toggleSound() {
+  let sound = document.getElementById("soundDesign");
+  let video = document.getElementById("video");
+  let music = document.getElementById("music");
+  if (sound.style.display === "none") {
+      sound.style.display = "block";
+      video.style.display = "none";
+      music.style.display = "none";
+  } else {
+    sound.style.display = "none";
+      }
+}
+
+function toggleVideo() {
+  let sound = document.getElementById("soundDesign");
+  let video = document.getElementById("video");
+  let music = document.getElementById("music");
+  if (video.style.display === "none") {
+      video.style.display = "block";
+      sound.style.display = "none";
+      music.style.display = "none";
+  } else {
+      video.style.display = "none";
+      }
+}
+
+function toggleMusic() {
+  let sound = document.getElementById("soundDesign");
+  let video = document.getElementById("video");
+  let music = document.getElementById("music");
+  if (music.style.display === "none") {
+      music.style.display = "block";
+      video.style.display = "none";
+      sound.style.display = "none";
+  } else {
+      music.style.display = "none";
+      }
+}
