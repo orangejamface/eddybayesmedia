9c00fb0f35f2a8f1ce4f262f5e25121bd36d2a22
orangejamface <orangejamface@gmail.com>
2022-02-24 22:16:37 -0500

    added some content for sound design and video section, FIXED BUG in toggle functions, by changing the what is being checked in my if else, rather than if (variable.style.display === none), I changed it to if (varibale.style.display !== block).  this stopped the need for two clicks on the toggle button when the page first loads. YAY!  I then managed to shorten my javascript by declaring the variables sound, video and music outside of the functions, as they are used in all three toggle functions and do not change this cut out 6 lines of code, at first it did not work but then i found out that if I moved my <script src="java.js></script> to the very end of the body in my html it functioned as normal.

--- java.js
+++ java.js
@@ -1,9 +1,12 @@
 
+
+
+  const sound = document.getElementById("soundDesign");
+  const video = document.getElementById("video");
+  const music = document.getElementById("music");
+
 function toggleSound() {
-  let sound = document.getElementById("soundDesign");
-  let video = document.getElementById("video");
-  let music = document.getElementById("music");
-  if (sound.style.display === "none") {
+  if (sound.style.display !== "block") {
       sound.style.display = "block";
       video.style.display = "none";
       music.style.display = "none";
@@ -12,11 +15,9 @@ function toggleSound() {
       }
 }
 
+
 function toggleVideo() {
-  let sound = document.getElementById("soundDesign");
-  let video = document.getElementById("video");
-  let music = document.getElementById("music");
-  if (video.style.display === "none") {
+  if (video.style.display !== "block") {
       video.style.display = "block";
       sound.style.display = "none";
       music.style.display = "none";
@@ -26,10 +27,7 @@ function toggleVideo() {
 }
 
 function toggleMusic() {
-  let sound = document.getElementById("soundDesign");
-  let video = document.getElementById("video");
-  let music = document.getElementById("music");
-  if (music.style.display === "none") {
+  if (music.style.display !== "block") {
       music.style.display = "block";
       video.style.display = "none";
       sound.style.display = "none";
@@ -37,3 +35,6 @@ function toggleMusic() {
       music.style.display = "none";
       }
 }
+
+
+// BELOW APPLIES TO VIDEO LOADING AND PLAYING
